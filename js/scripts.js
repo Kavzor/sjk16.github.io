8d2269d61d84005080755b54436c8eae3532364f
mfris <martin.frisk9103@gmail.com>
2016-08-23 10:05:45 +0200

    changed to work for sjk16

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function markWeek(jQuery) {
   var currentWeek = date.getWeek();
 
   //var startWeek2016 = $("h3:contains(2016) + table tbody td:first").text(); // 2
-  var currentWeekTableRow = currentWeek; //hårdkodat för att kompensera för veckorna under sommaren
+  var currentWeekTableRow = currentWeek; 
 
 
   // Style weeks in the past in 2016
