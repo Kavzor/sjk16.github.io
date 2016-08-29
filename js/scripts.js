a635f4e6db05552954f9a9e119c45d899a37f204
mfris <martin.frisk9103@gmail.com>
2016-08-29 19:18:46 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ function markWeek(jQuery) {
   //var startWeek2016 = $("h3:contains(2016) + table tbody td:first").text(); // 2
 
  var startWeek2016 = $("h3:contains(2016) + table tbody td:first").text(); // 2
-  var currentWeekTableRow = currentWeek- startWeek2016 -16; //hårdkodat för att kompensera för veckorna under sommaren
+  var currentWeekTableRow = currentWeek- startWeek2016 -12; //hårdkodat för att kompensera för veckorna under sommaren
 
 
   // Style weeks in the past in 2016
